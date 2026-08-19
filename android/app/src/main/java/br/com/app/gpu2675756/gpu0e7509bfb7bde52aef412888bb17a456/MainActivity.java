5225435f9a24db29e27f6bdcd1de1c2b7176bda9
wesleyhard12 <134656570+wesleyhard12@users.noreply.github.com>
2026-08-19 18:18:29 -0300

    fix(android): remove barra de rolagem nativa branca do webview

--- android/app/src/main/java/br/com/app/gpu2675756/gpu0e7509bfb7bde52aef412888bb17a456/MainActivity.java
+++ android/app/src/main/java/br/com/app/gpu2675756/gpu0e7509bfb7bde52aef412888bb17a456/MainActivity.java
@@ -1,6 +1,7 @@
 package br.com.app.gpu2675756.gpu0e7509bfb7bde52aef412888bb17a456;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 import androidx.activity.EdgeToEdge;
 import androidx.core.splashscreen.SplashScreen;
 import com.getcapacitor.BridgeActivity;
@@ -15,5 +16,12 @@ public class MainActivity extends BridgeActivity {
         EdgeToEdge.enable(this);
         
         super.onCreate(savedInstanceState);
+
+        // Desabilita as barras de rolagem nativas do WebView
+        WebView webView = getBridge().getWebView();
+        if (webView != null) {
+            webView.setVerticalScrollBarEnabled(false);
+            webView.setHorizontalScrollBarEnabled(false);
+        }
     }
 }
