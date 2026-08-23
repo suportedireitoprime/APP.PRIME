705458b15e03e2e6bc7295a4e6afaf81afba86dd
wesleyhard12 <134656570+wesleyhard12@users.noreply.github.com>
2026-08-23 15:21:56 -0300

    feat: configurar pg_cron para execucao da edge function do blog

--- supabase/migrations/20260822210000_blog_edicao_crons.sql
+++ supabase/migrations/20260822210000_blog_edicao_crons.sql
@@ -1,9 +1,7 @@
-DO $$
-DECLARE
-  fn_url_base text := 'https://dnjrgpldcwcpoywamorr.supabase.co/functions/v1';
-  anon text := 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRuanJncGxkY3djcG95d2Ftb3JyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUxNTE3MjksImV4cCI6MjAyODc1MTcyOX0.zS35X7RjE1bWkZ0B5R3qB9O8yTj5X7RjE1bWkZ0B5R3qB9O8yTj'; -- This would need to be actual anon key if it was hardcoded, but wait!
-BEGIN
-  -- Better to use a simpler approach without hardcoded anon key if we can't reliably get it here,
-  -- but actually we can just invoke it from the admin panel if we create an RPC.
-END;
-$$;
+select cron.schedule('blog-edicao-runner-cron','*/15 * * * *',$$
+  select net.http_post(
+    url:='https://dnjrgpldcwcpoywamorr.supabase.co/functions/v1/blog-edicao-runner',
+    headers:='{"Content-Type": "application/json", "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRuanJncGxkY3djcG95d2Ftb3JyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI2ODYxMzMsImV4cCI6MjA5ODI2MjEzM30.GuZuUn1ITbjsTYi_SjL-eFSCxdxxs3rUASArbMf62O0"}'::jsonb,
+    body:='{}'::jsonb
+  );
+$$);
