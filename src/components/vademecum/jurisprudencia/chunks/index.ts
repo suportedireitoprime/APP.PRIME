3f50bee97237f8a073b2f264f7f2413006a041a6
wesleyhard12 <134656570+wesleyhard12@users.noreply.github.com>
2026-09-05 20:38:06 -0300

    fix(jurisprudencia): adicionar aliases de exportação no index dos chunks

--- src/components/vademecum/jurisprudencia/chunks/index.ts
+++ src/components/vademecum/jurisprudencia/chunks/index.ts
@@ -4,3 +4,10 @@ export * from './JurisprudenciaFiltrosBar';
 export * from './JurisprudenciaCategoriaList';
 export * from './JurisprudenciaCategoriaSheet';
 export * from './JurisprudenciaDetalheSheet';
+
+// Aliases
+export { JurisprudenciaUnsupportedCard as JurisUnsupportedCard } from './JurisprudenciaUnsupportedCard';
+export { JurisprudenciaFiltrosBar as JurisFiltrosBar } from './JurisprudenciaFiltrosBar';
+export { JurisprudenciaCategoriaList as JurisCategoriaList } from './JurisprudenciaCategoriaList';
+export { JurisprudenciaCategoriaSheet as JurisCategoriaSheet } from './JurisprudenciaCategoriaSheet';
+export { JurisprudenciaDetalheSheet as JurisDetalheSheet } from './JurisprudenciaDetalheSheet';
