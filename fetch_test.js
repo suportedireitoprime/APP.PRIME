00906330b7d16aac7b05a7b0e5fa25187607a9be
wesleyhard12 <134656570+wesleyhard12@users.noreply.github.com>
2026-09-16 03:47:03 -0300

    chore: remove fetch_test.js

--- fetch_test.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const env = fs.readFileSync('.env', 'utf8').split('\n').reduce((acc, line) => {
-  const [k, v] = line.split('=');
-  if(k) acc[k] = v?.replace(/["'\r]/g, '');
-  return acc;
-}, {});
-fetch(env.VITE_SUPABASE_URL + '/rest/v1/radar_deputados?select=dados_json&limit=1', {
-  headers: { apikey: env.VITE_SUPABASE_ANON_KEY }
-}).then(r => r.json()).then(d => console.log(JSON.stringify(d)));
