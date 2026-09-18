2bf6d9c4aa9d52e8edd49574170c29b93e43fe96
wesleyhard12 <134656570+wesleyhard12@users.noreply.github.com>
2026-09-18 16:06:30 -0300

    fix: adiciona mute=1 para garantir autoplay no navegador

--- src/pages/VideoaulasCanalDetail.tsx
+++ src/pages/VideoaulasCanalDetail.tsx
@@ -197,7 +197,7 @@ export default function VideoaulasCanalDetail() {
               <iframe
                 width="100%"
                 height="100%"
-                src={`https://www.youtube.com/embed/${playingVideo.id}?autoplay=1&playsinline=1`}
+                src={`https://www.youtube.com/embed/${playingVideo.id}?autoplay=1&mute=1&playsinline=1`}
                 title={playingVideo.title}
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
