de15e34b558c6acee781fa583bd9a0074b36170b
wesleyhard12 <134656570+wesleyhard12@users.noreply.github.com>
2026-09-21 02:07:53 -0300

    fix(vademecum): throw error when NativeVadeMecumPlugin is unavailable to ensure web fallback works and prevents modal from instantly closing

--- src/plugins/NativeVadeMecumPlugin.ts
+++ src/plugins/NativeVadeMecumPlugin.ts
@@ -31,13 +31,15 @@ const RawNativeVadeMecumPlugin = registerPlugin<NativeVadeMecumPluginInterface>(
 
 export const NativeVadeMecumPlugin: NativeVadeMecumPluginInterface = {
   async openArtigo(options) {
-    if (!Capacitor.isPluginAvailable('NativeVadeMecumPlugin')) return;
+    if (!Capacitor.isPluginAvailable('NativeVadeMecumPlugin')) {
+      throw new Error('NativeVadeMecumPlugin not available');
+    }
     return RawNativeVadeMecumPlugin.openArtigo(options);
   },
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   addListener: async (eventName: any, listenerFunc: any): Promise<PluginListenerHandle> => {
     if (!Capacitor.isPluginAvailable('NativeVadeMecumPlugin')) {
-      return { remove: async () => {} } as unknown as PluginListenerHandle;
+      throw new Error('NativeVadeMecumPlugin not available');
     }
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     return (RawNativeVadeMecumPlugin as any).addListener(eventName, listenerFunc);
